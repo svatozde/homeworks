4439a68e19aa00e24b006dca10e76c44fbe2deac
svatozde <zdenek.svaton25@gmail.com>
2013-10-07 18:30:14 +0200

    Update Homework1.java

--- Homework1.java
+++ Homework1.java
@@ -1,51 +1,90 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 
-class Homework1 implements Mergesort {
 
-    public int[] getFirstHalfOf(int[] array) {
-        Double d = (double)(array.length/2D);
-        return Arrays.copyOfRange(array, 0, (int)Math.ceil(d));
-    }
 
-    public int[] getSecondHalfOf(int[] array) {
-        Double d = (double)(array.length/2D);
-        return Arrays.copyOfRange(array, (int)Math.ceil(d), array.length);
-    } 
 
-    public int[] merge(int[] firstHalf, int[] secondHalf) {
-        ArrayList<Integer> tmp = new ArrayList<Integer>();
-        int prvniPole = 0;
-        int druhePole = 0;
+public class Homework1 implements Mergesort {
 
-        while (prvniPole < firstHalf.length && druhePole < secondHalf.length) {
-            if (firstHalf[prvniPole] < secondHalf[druhePole]) {
-                tmp.add(firstHalf[prvniPole]);
-                prvniPole++;
-            } else {
-                tmp.add(secondHalf[druhePole]);
-                druhePole++;
-            }
-        }
-        for (int i = prvniPole; i < firstHalf.length; i++) {
-            tmp.add(firstHalf[i]);
-        }
-        for (int i = druhePole; i < secondHalf.length; i++) {
-            tmp.add(secondHalf[i]);
-        }
 
-        int[] vysledek = new int[tmp.size()];
-        for (int i = 0; i < tmp.size(); i++) {
-            vysledek[i] = tmp.get(i);
-        }
+	public Homework1(){
+		super();
+		//public constructor
+	}
+	
+	@Override
+	public int[] getFirstHalfOf(int[] array) {
+		return Arrays.copyOfRange(array, 0, (int) Math.round((1.0*array.length)/2));
+	}
 
-        return vysledek;
-    }
+	@Override
+	public int[] getSecondHalfOf(int[] array) {
 
-    public int[] mergesort(int[] array) {
-        if (array.length < 2) return array.clone();
-        return merge(mergesort(getFirstHalfOf(array)),
-                     mergesort(getSecondHalfOf(array))).clone();
-    }
+		return Arrays.copyOfRange(array, (int) Math.round((1.0*array.length)/2),array.length);
+	}
+
+	@Override
+	public int[] merge(int[] firstHalf, int[] secondHalf) {
+		int[] ret = new int[firstHalf.length + secondHalf.length];
+		//secondHalfIndex
+		int sHi = 0; 
+		//firstHalfIndex
+		int fHi = 0;
+		//retIndex
+		int rI = 0;	
+		while(rI<ret.length){
+			if(sHi >= secondHalf.length){
+				ret[rI++] = firstHalf[fHi++];
+			}else if(fHi >= firstHalf.length){
+				ret[rI++] = secondHalf[sHi++];
+			}else if(firstHalf[fHi] < secondHalf[sHi]){
+				ret[rI++] = firstHalf[fHi++];
+			}else{
+				ret[rI++] = secondHalf[sHi++];
+			}
+		}
+
+		return ret;
+	}
+
+	@Override
+	public int[] mergesort(int[] array) {
+		int[] fH = getFirstHalfOf(array);
+		int[] sH = getSecondHalfOf(array);
+
+		if(fH.length > 1){
+			fH = mergesort(fH);
+		}
+
+		if(sH.length > 1){
+			sH = mergesort(sH);
+		}
+
+		return merge(fH, sH);
+	}
+
+
+}
+
+/*
+* Naimplementujte třídu Homework1 implementující rozhraní Mergesort.
+*/
+
+interface Mergesort { 
+
+   // vrati nesetridenou kopii prvni poloviny (horni celou cast) pole array
+
+   int[] getFirstHalfOf(int[] array); 
+
+   // vrati nesetridenou kopii druhe poloviny (dolni celou cast) pole array
+
+   int[]getSecondHalfOf(int[] array); 
+
+   // slije prvky v firstHalf a secondHalf do jednoho pole a vrati ho
+
+   int[]merge(int[] firstHalf, int[] secondHalf); 
+
+   // vrati setridenou kopii pole array
+
+   int[]mergesort(int[] array); 
 
 }
