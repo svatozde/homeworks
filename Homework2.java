0441c65e9fdfb969fee00dce5c7a33fa3778bf6c
svatozde <zdenek.svaton25@gmail.com>
2013-10-23 08:53:27 +0200

    Update Homework2.java

--- Homework2.java
+++ Homework2.java
@@ -7,16 +7,14 @@ public class homework2 implements InterSearch {
     @Override
     public int search(int first, int last, int what, int[] data) {
 		
-    	 if(data[first] == what) return first; //nalezli jsme, treba kontrolovat kvuli deleni nulou dale v algoritmu
-    	    else if(first == last || data[first] ==  data[last]) return -1; //zarazka rekurze
+    	 if(data[first] == what) return first; 
+    	    else if(first == last || data[first] ==  data[last]) return -1; 
 
-    	    //provedeme odhad pozice hledaneho cisla na zaklade soucasneho rozsahu pozic, hodnot mezi a hledane hodnoty
+    	    
     	    int index = first + Math.round((last - first)/(data[last] - data[first])) * (what - data[first]);
     	    
-    	    if(data[index] == what) return index;//cislo jsme nalezli
-    	    //nenalezli jsme, pokracujeme v prave polovine, pokud je hledane cislo vyssi nez predel
+    	    if(data[index] == what) return index;
     	    else if(data[index] < what) return search( index + 1, last, what,data);
-    	    //nenalezli jsme, pokracujeme v leve polovine, pokud je hledane cislo nizsi nez predel
     	    else return search( first, index - 1,what,data);
     	
     }
